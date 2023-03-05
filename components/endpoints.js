d1063e5d3f281043fbad629dc2bcc3b4a3b8ad51
Nick M <nick.montagni@thephoenixgroup.com>
2023-03-05 11:25:16 +0000

    added dashboard

--- components/endpoints.js
+++ components/endpoints.js
@@ -1,11 +1,14 @@
 // The photos API endpoint
-let photosURL = 'https://path-to-your.workers.dev';
+let photosURL = 'https://royal-salad-a3f5.nickmontagni.workers.dev';
 
 // The stripe API endpoint
-let stripeURL = 'https://path-to-stripe.workers.dev';
+let stripeURL = 'https://delicate-thunder-strip.nickmontagni.workers.dev';
 
 // The authentication API endpoint
 let authURL = 'https://ask-heart-user.nickmontagni.workers.dev';
 
+// The dashboard API endpoint
+let dashURL = 'https://round-glitter-dashboard.nickmontagni.workers.dev';
 
-export {photosURL, stripeURL, authURL};
+
+export {photosURL, stripeURL, authURL, dashURL};
